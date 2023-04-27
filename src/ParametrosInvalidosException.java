5caa7838580bef3c1e01b83bcdcdf70d245ad850
Esdras <esdrasbalves10@gmail.com>
2023-04-26 22:57:24 -0300

    first commit

---
+++ src/ParametrosInvalidosException.java
@@ -0,0 +1,5 @@
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException() {
+        super("O segundo parâmetro deve ser maior que o primeiro");
+    }
+}
